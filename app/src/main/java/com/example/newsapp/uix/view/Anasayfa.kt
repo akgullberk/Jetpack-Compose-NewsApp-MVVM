3f3608c9ab051ed1c697dfe5f370bc8532ac5d3b
Berk Akgül <berkak_gul51@hotmail.com>
2024-12-09 19:50:00 +0300

    anasayfa uı

--- app/src/main/java/com/example/newsapp/uix/view/Anasayfa.kt
+++ app/src/main/java/com/example/newsapp/uix/view/Anasayfa.kt
@@ -2,8 +2,10 @@ package com.example.newsapp.uix.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +17,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
@@ -22,39 +25,70 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.newsapp.R
 import com.example.newsapp.data.entity.HaberlerItem
 import com.example.newsapp.viewmodel.AnasayfaViewModel
 import com.skydoves.landscapist.glide.GlideImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Anasayfa(anasayfaViewModel: AnasayfaViewModel){
+    var selectedCategory by remember { mutableStateOf("general") } // Default category
     var selectedItem by remember { mutableStateOf(0) }
 
     // Observe the news list and error message from the ViewModel
     val newsList by anasayfaViewModel.newsList.observeAsState(emptyList())
     val errorMessage by anasayfaViewModel.errorMessage.observeAsState("")
 
-    // Fetch news if it's not already loaded
-    if (newsList.isEmpty()) {
-        anasayfaViewModel.fetchNews(country = "tr", tag = "general") // Example parameters
+    // Fetch news whenever the selected category changes
+    LaunchedEffect(selectedCategory) {
+        anasayfaViewModel.fetchNews(country = "tr", tag = selectedCategory)
     }
 
     Scaffold(
         containerColor = Color(0xFF1b1a1f),
+        topBar = {
+            TopAppBar(
+                title = { Icon(painter = painterResource(id = R.drawable.calendar), contentDescription = "", tint = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0xFF1b1a1f)),
+                actions = {
+                    Row {
+                        listOf("Tümü" to "general", "Spor" to "sport", "Ekonomi" to "business", "Teknoloji" to "technology","Magazin" to "magazine").forEach { (label, tag) ->
+                            TextButton(onClick = { selectedCategory = tag }) {
+                                Text(
+                                    text = label,
+                                    color = if (selectedCategory == tag) Color.White else Color(0xFF898c8a),
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        },
         bottomBar = {
             BottomAppBar(
                 containerColor = Color(0xFF0b0e13)
@@ -108,29 +142,48 @@ fun Anasayfa(anasayfaViewModel: AnasayfaViewModel){
 
 @Composable
 fun NewsItem(haberlerItem: HaberlerItem) {
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.DarkGray)
-            .padding(8.dp)
+        .clip(RoundedCornerShape(12.dp))
     ) {
-        // Display the image using Landscapist-Glide
-        GlideImage(
-            imageModel = haberlerItem.image, // Replace with your image URL field
-            contentDescription = "News Image",
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(150.dp)
-                .background(Color.Gray),
-            contentScale = ContentScale.Crop
-        )
+                .background(Color(0xFF171e26))
 
-        Spacer(modifier = Modifier.height(8.dp))
+        ) {
+            Box {
+                GlideImage(
+                    imageModel = haberlerItem.image, // Replace with your image URL field
+                    contentDescription = "News Image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .background(Color.Gray),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            // Display the image using Landscapist-Glide
 
-        Text(
-            text = haberlerItem.name,
-            color = Color.White,
-            style = MaterialTheme.typography.headlineMedium
-        )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Box(
+                modifier = Modifier.padding(16.dp)
+            ) {
+
+
+                Text(
+                    text = haberlerItem.name,
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 25.sp
+                )
+            }
+
+        }
     }
+
 }
